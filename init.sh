53ce3493f3cde7278dbbff16716afcdd7b688c13
mphung97 <mphung97@github.com>
2024-05-05 18:56:51 +0700

    devbox commit

--- init.sh
+++ init.sh
@@ -1,15 +1,14 @@
-shell=`ps -p $$ | awk 'NR>1  {print $4}' | sed 's/-//g'`
+shell=$(ps -p $$ | awk 'NR>1  {print $4}' | sed 's/-//g')
 SCRIPT_PATH="$HOME/.local/share/devbox/global/current"
 
 case $(basename $shell) in
-     "zsh" )
-            . $DEVBOX_GLOBAL_ROOT/zsh/.zshrc
-           ;;
-     "bash" )
-            . $DEVBOX_GLOBAL_ROOT/bash/.bashrc
-           ;;
-     * )
-           ;;
+"zsh")
+	. $DEVBOX_GLOBAL_ROOT/zsh/.zshrc
+	;;
+"bash")
+	. $DEVBOX_GLOBAL_ROOT/bash/.bashrc
+	;;
+*) ;;
 esac
 
 # zoxide
@@ -23,12 +22,12 @@ alias cat='bat'
 # devbox helpers
 alias dbr='devbox run'
 alias cddevbox='cd $DEVBOX_GLOBAL_ROOT'
-
+# nvim
+alias n='nvim'
+# exa
 alias ld='eza -lD'
 alias lf='eza -lF --color=always | grep -v /'
 alias lh='eza -dl .* --group-directories-first'
 alias ll='eza -al --group-directories-first'
 alias ls='eza -alF --color=always --sort=size | grep -v /'
 alias lt='eza -al --sort=modified'
-
-
